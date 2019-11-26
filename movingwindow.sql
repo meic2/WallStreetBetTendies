e000e7c479233c611ca8321c30aca17e6023f901
mei chen <meic2@illinois.edu>
2019-11-26 00:02:10 -0600

    update on af1

--- movingwindow.sql
+++ movingwindow.sql
@@ -1,75 +1,60 @@
-CREATE OR REPLACE FUNCTION calVolatility(dateStart timestamp without time zone, 
+CREATE OR REPLACE FUNCTION calVolatility2(dateStart timestamp without time zone, 
 										 dateEnd timestamp without time zone) 
-	RETURNS TABLE(
-	stock_time timestamp without time zone,
-              			stock_name VARCHAR,
-               	  moving_10_volatility INT,
-         	   	  moving_30_volatility INT
-)	AS $$
+	RETURNS table (j json) 	AS 
 
+	$$
 	BEGIN
-		
-		RETURN QUERY
-
-		WITH derived AS (
-		select stock_symbol, ts, close_price, log( close_price / price_prev ) as return
-		from (
-		select stock_symbol
-		    , ts
-		    , close_price
-		    , (
-		         select close_price
-		         from stocktickdata b
-		         where b.stock_symbol = a.stock_symbol
-		         and b.ts < a.ts
-		         order by b.ts desc
-		         limit 1
-		      ) as price_prev
-		from stocktickdata AS a
-		) derived),
-		
-		
-		moving_avolatility as (
-			SELECT stock_symbol, ts,
-			
-			CASE WHEN
-			
-				ROW_NUMBER() OVER (
-						PARTITION BY stock_symbol
-						ORDER BY ts) > 9 
-				THEN
-				 stddev_samp(return) OVER (PARTITION BY stock_symbol ORDER BY ts
-											ROWS BETWEEN 10 PRECEDING AND CURRENT ROW)
-									
-				END AS moving_10_volatility,
-			
-			CASE WHEN
-				ROW_NUMBER() OVER (
-						PARTITION BY stock_symbol
-						ORDER BY ts) > 29
-				THEN
-				 stddev_samp(return) OVER (PARTITION BY stock_symbol ORDER BY ts
-											ROWS BETWEEN 30 PRECEDING AND CURRENT ROW)
-									
-				END AS moving_30_volatility
-				FROM derived
-				WHERE ts >= dateStart
-				AND ts <= dateEnd
-		)
-		
-				SELECT moving_avolatility.ts ::timestamp without time zone,
-				moving_avolatility.stock_symbol ::VARCHAR, 
-				moving_avolatility.moving_10_volatility ::INT, 
-				moving_avolatility.moving_30_volatility ::INT
-				FROM a
-		
+	RETURN QUERY
 	
+	WITH derived AS (
+	select stock_symbol, ts, close_price, log( close_price / price_prev ) as return
+	from (
+	select stock_symbol
+	    , ts
+	    , close_price
+	    , (
+	         select close_price
+	         from stocktickdata b
+	         where b.stock_symbol = a.stock_symbol
+	         and b.ts < a.ts
+	         order by b.ts desc
+	         limit 1
+	      ) as price_prev
+	from stocktickdata AS a
+	) derived
+	),
+	
+	moving_volatility as (
+		SELECT stock_symbol, ts,
+		
+		CASE WHEN
+		
+			ROW_NUMBER() OVER (
+					PARTITION BY stock_symbol
+					ORDER BY ts) > 9 
+			THEN
+			 stddev_samp(return) OVER (PARTITION BY stock_symbol ORDER BY ts
+										ROWS BETWEEN 10 PRECEDING AND CURRENT ROW)
+								
+			END AS moving_10_volatility,
+		
+		CASE WHEN
+			ROW_NUMBER() OVER (
+					PARTITION BY stock_symbol
+					ORDER BY ts) > 29
+			THEN
+			 stddev_samp(return) OVER (PARTITION BY stock_symbol ORDER BY ts
+										ROWS BETWEEN 30 PRECEDING AND CURRENT ROW)
+								
+			END AS moving_30_volatility
+			FROM derived)
+
+		SELECT row_to_json(a) FROM (SELECT * FROM moving_volatility) a;
 	END;
 	$$ LANGUAGE plpgsql;
-	
-	
-SELECT public.calVolatility('1999-11-01 00:00:00', '1999-12-07 00:00:00')
-FROM stocktickdata;
+
+
+SELECT calVolatility2('1999-11-01 00:00:00', '2001-12-07 00:00:00');
 
 
 
